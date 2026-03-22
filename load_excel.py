fc7e1d36a2851545991b9fcce6f439396d6e69f2
Rony Gozes <ronygozes@gmail.com>
2026-03-22 05:14:39 +0200

    added new files missing from last commit

---
+++ load_excel.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+
+def load_and_clean(path):
+    df = pd.read_excel(path, header=None)
+    df.columns = ["סעיף", "תאור", "יח", "כמות", "מחיר", 'סה"כ']
+   
+    df = df.replace(r'^\s*$', None, regex=True)
+    df = df[df["סעיף"].notna()]
+    df = df.drop(df.index[0], axis=0)
+    df = df.reset_index(drop=True)
+    df = df.drop(['סה"כ', 'כמות'], axis=1)
+
+    df["chapter"] = df["סעיף"].str.extract(r"^(\d{2})")
+    # headers = df[df["סעיף"].str.count(r"\.") == 0]
+    items = df[df["סעיף"].str.count(r"\.") == 2]
+    chapters = {
+        ch: items[items["chapter"] == ch]
+        for ch in items["chapter"].unique()
+    }    
+    return chapters
